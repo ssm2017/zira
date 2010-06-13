23af5f6edd7b468abbd183c04d59bd4c508dc3c7
jhurliman@jhurliman.org <jhurliman@jhurliman.org@7241c5be-0a0a-11df-a92b-075ba6b65aa2>
2010-06-13 10:35:03 +0000

    * Initial commit of the VWRAP Launcher, a glue app to go from http://tools.ietf.org/html/draft-hamrick-vwrap-launch documents on the web to a virtual world rich client (viewer)
    git-svn-id: http://openmetaverse.googlecode.com/svn/trunk@426 7241c5be-0a0a-11df-a92b-075ba6b65aa2

---
+++ Launcher/LaunchDocument.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using OpenMetaverse.StructuredData;
+
+namespace VWRAPLauncher
+{
+    public class LaunchDocument
+    {
+        public string AccountName;
+        public string Name;
+        public string LoginUrl;
+        public string Region;
+        public bool IsLoginUrlCapability;
+
+        public string FirstName
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(Name) && Name.Contains(" "))
+                    return Name.Substring(0, Name.IndexOf(' '));
+                return String.Empty;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(Name) && Name.Contains(" "))
+                    return Name.Substring(Name.IndexOf(' ') + 1);
+                return String.Empty;
+            }
+        }
+
+        public static LaunchDocument FromFile(string path)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    OSDMap launchMap = OSDParser.Deserialize(stream) as OSDMap;
+
+                    if (launchMap != null)
+                    {
+                        LaunchDocument document = new LaunchDocument();
+
+                        document.LoginUrl = launchMap["loginurl"].AsString();
+
+                        // Not a valid launch doc without a loginurl
+                        if (String.IsNullOrEmpty(document.LoginUrl))
+                            return null;
+
+                        document.Region = launchMap["region"].AsString();
+
+                        OSDMap authenticatorMap = launchMap["authenticator"] as OSDMap;
+                        if (authenticatorMap != null)
+                        {
+                            document.IsLoginUrlCapability = (authenticatorMap["type"].AsString() == "capability");
+                        }
+
+                        OSDMap identifierMap = launchMap["identifier"] as OSDMap;
+                        if (identifierMap != null)
+                        {
+                            document.AccountName = launchMap["account_name"].AsString();
+                            document.Name = launchMap["name"].AsString();
+
+                            // Legacy support
+                            if (String.IsNullOrEmpty(document.Name))
+                            {
+                                string first = launchMap["first_name"].AsString();
+                                string last = launchMap["last_name"].AsString();
+
+                                document.Name = (first + " " + last).Trim();
+                            }
+                        }
+
+                        return document;
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return null;
+        }
+    }
+}
